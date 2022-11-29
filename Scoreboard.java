48e14a0a3dfcc547e4726e215825dc6212994d7b
sarah <sjschindler97@gmail.com>
2022-11-29 15:53:19 -0500

    Fixed a hash map bug in Scoreboard.

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 import javax.swing.*;
 
@@ -28,14 +27,19 @@ public class Scoreboard {
 
     private HashMap<String, Integer> dataMap = new HashMap<>(); //map to contain read in file data
 
-    private HashMap<String, Integer> updatedMap; //map to sort and update the scoreboard
+    private HashMap<String, Integer> updatedMap = new HashMap<>(); //map to sort and update the scoreboard
+
+    private ArrayList<String> topKeys;
+
 
     /**
-     * Default constructor that instantiates the number of guesses to null (0)
+     * Default constructor that instantiates the number of guesses to 6
+     * This will return a 0 (null) score in calculateScore
      */
     public void Scoreboard(){
-        guesses = 0;
+        guesses = 6;
     }
+
 
     /**
      * Parameterized constructor that instantiates the number of guesses
@@ -45,7 +49,12 @@ public class Scoreboard {
         guesses = i;
     }
 
-    //create a scoreboard file if one does not exist
+
+    /**
+     * Create a file object with the universal path
+     * Check if the file path already exists in the users directory
+     * If not, create the scoreboard file
+     */
     public void createScoreboard(){
         scoreboardFile = new File(scoreboardPath);
 
@@ -61,7 +70,13 @@ public class Scoreboard {
         }
     }
 
-    //read in the file data
+
+    /**
+     * Read in the lines of the scoreboard file and add them to a hash map for sorting and updating
+     * Lines are split at the spaces between the username and score
+     * Hash map key = String name
+     * Hash map values = Integer score
+     */
     public void readScoreboard(){
         String line;
 
@@ -72,20 +87,22 @@ public class Scoreboard {
                 line = scan.nextLine();
                 //System.out.println(line);
 
-                String[] updatedMap = line.split(" ");
-                //System.out.println(updatedMap[0] + " " + updatedMap[1]);
+                String[] dataArray = line.split(" ");
 
-                dataMap.put(updatedMap[0], Integer.valueOf(updatedMap[1]));
+                dataMap.put(dataArray[0], Integer.valueOf(dataArray[1]));
             }
         }catch (IOException e){
             System.out.println("Error in reading scoreboard file.");
         }
     }
 
+
     /**
      * Calculate the users score based on their number of guesses
      */
-    public void calculateScore(){
+    public Integer calculateScore(){
+
+        //System.out.println("Guesses: " + guesses);
 
         if(guesses == 1){
             score = 10;
@@ -102,16 +119,23 @@ public class Scoreboard {
         }else{
             score = 0;
         }
+
+        return score;
     }
 
-    //sort the scoreboard
+
+    /**
+     * Sort the scores in the scoreboard hash map
+     * @param n how many high scores to record
+     */
     public void sortScores(int n){
 
         // make an arraylist to store our winning players in
-        ArrayList<String> topKeys = new ArrayList<>();
+        topKeys = new ArrayList<>();
 
         // make updatedMap of hash map ( so we can delete elements as we pull highest )
-        updatedMap = dataMap;
+        updatedMap = new HashMap<>(dataMap);
+
         // for the top x scores
         for(int i = 0; i < n; i++){
             Integer highestValue = 0;
@@ -127,59 +151,93 @@ public class Scoreboard {
             topKeys.add(highestKey);
             updatedMap.remove(highestKey);
         }
-        dataMap = updatedMap;
+        //dataMap = updatedMap; //update the original map by making it equal to the sorted map
     }
 
-    //print the keys and values of the hash map
-    public void printMap(){
-        for (String s : dataMap.keySet());
-    }
 
-    //write new high scores in
+    /**
+     * Check if the users score is higher than the scores on the scoreboard
+     * If it is, add it to the dataMap and re-sort
+     */
     public void addHighScore(){
         Scanner scanName = new Scanner(System.in);
 
         //call the users score
         calculateScore();
+        //System.out.println("Score: " + score);
 
         try {
+            //boolean check to save if the users score belongs on the scoreboard
+            boolean checkScore = false;
+
             //loop through the sorted hash map of high scores
-            for (String s : updatedMap.keySet()) {
+            for (String s : dataMap.keySet()) {
                 //if the users score is higher than the scores in the scoreboard
-                if (score > updatedMap.get(s)) {
-                    //take in user input for their name
-                    System.out.println("Input a name for the scoreboard: ");
-                    String name = scanName.nextLine();
-                    updatedMap.put(name, score);
+                if (score > dataMap.get(s)) {
+                   checkScore = true;
                 }
+            }
+
+            //if the users score belongs on the scoreboard, take in their name to add
+            if(checkScore){
+                //take in user input for their name
+                System.out.println("Input a name for the scoreboard: ");
+                String name = scanName.nextLine();
+
+                //check if the name is already used as a key, if so have the user input a new name
+                while(dataMap.containsKey(name)){
+                    System.out.println("Input name is taken. Please input a different name: ");
+                    name = scanName.nextLine();
+                }
+
+                //add the new high score to the scoreboard hash map
+                dataMap.put(name, score);
             }
 
             //re-sort the score with the new score added
             sortScores(3);
-            dataMap = updatedMap;
+            for(String s : dataMap.keySet()){
+                System.out.println("Sorted map in addScore: " + s + " " + dataMap.get(s));
+            }
 
         }catch (NullPointerException npe){
             System.out.println("Error: updated scoreboard map is empty.");
         }
     }
 
-    //write the updated scoreboard hashmap back to the file
+
+    /**
+     * Write the updated scoreboard map back to the file
+     */
     public void updateScoreboard(){
         try {
             FileWriter writeTo = new FileWriter(scoreboardFile);
 
-            for(String s: updatedMap.keySet()){
-                writeTo.write(s + " " + updatedMap.get(s));
-                writeTo.close();
+            for(String s: topKeys) {
+                writeTo.write(s + " " + dataMap.get(s) + "\n");
             }
+            writeTo.close();
+
+
         }catch (IOException e){
             System.out.println("Error writing to scoreboard file.");
         }
     }
 
+
     /**
-     * Converts the scoreboard hashmap to an array list for sending to the 
-     * ScoreboardGraphics class
+     * Prints the values of dataMap that contains the high scores
+     */
+    public void printMap(){
+
+        for (String s : dataMap.keySet()){
+            System.out.println(s + " : " + dataMap.get(s));
+        }
+    }
+
+
+    /**
+     * Converts the scoreboard hashmap to an array list for sending to the ScoreboardGraphics class
      */
     public void sendToGUI(){
 
@@ -194,6 +252,7 @@ public class Scoreboard {
         ScoreboardGraphics sg = new ScoreboardGraphics(scoreboardFrame, mapList);
     }
 
+
     /**
      * Master scoreboard class, calls all necessary methods
      */
@@ -204,16 +263,36 @@ public class Scoreboard {
         calculateScore(); // calculate the users score based on guesses used
         addHighScore(); //check if users score is a high score, if so add to map and re-sort
         sendToGUI(); //convert scoreboard to array list, send to scoreboard GUI
+        updateScoreboard(); //write the updated scoreboard back to the file
     }
 }
+
+
+
+
 
 class tester{
 
     private static Scoreboard sc = new Scoreboard();
 
     public static void main(String[] args){
-        sc.masterScoreboard();
+
+        sc.Scoreboard(1);
+
+        sc.createScoreboard();
+
+        sc.readScoreboard();
+
+        sc.calculateScore();
+
+        sc.addHighScore();
+
+        sc.updateScoreboard();
+
+
+
+
+
+        //sc.masterScoreboard();
     }
-
-
 }
